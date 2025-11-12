c396258d2e1a1fb7de5cb825ccfa683b2a233828
monir6163 <monirhossain6163@gmail.com>
2025-11-12 23:00:23 +0600

    as const

--- typescript-learning/src/asConst.ts
+++ typescript-learning/src/asConst.ts
@@ -15,3 +15,24 @@ const permissionFunction = (role: keyof typeof UserRoles) => {
 };
 const result = permissionFunction(UserRoles.Admin);
 console.log(result);
+
+// key value not same approch
+const UserRoles1 = {
+  Admin: "admin",
+  User: "user",
+  Modaretor: "modaretor",
+} as const;
+
+const permissionFunction1 = (
+  role: (typeof UserRoles1)[keyof typeof UserRoles1]
+) => {
+  if (role === UserRoles1.Admin) {
+    return `You are a ${UserRoles1.Admin}`;
+  } else if (role === UserRoles1.Modaretor) {
+    return `You are a ${UserRoles1.Modaretor}`;
+  } else {
+    return `You are a ${UserRoles1.User}`;
+  }
+};
+const result1 = permissionFunction1(UserRoles1.Admin);
+console.log(result1);
