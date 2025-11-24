9f391e4405f017a9e064d04579e2b0d577ca20f3
monir6163 <monirhossain6163@gmail.com>
2025-11-24 11:57:56 +0600

    nodejs learn

---
+++ nodejs-learn/fileOrganizer.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const path = require("path");
+
+const sourceDir = path.join(__dirname, "./output", "messy-files");
+const organizedDir = path.join(__dirname, "./output", "organized");
---
+++ nodejs-learn/os.js
@@ -0,0 +1,6 @@
+const os = require("os");
+console.log("Platform Details: ", os.platform());
+console.log("Arch Details: ", os.arch());
+// console.log("Cpus: ", os.cpus());
+console.log("Type: ", os.type());
+console.log("Release: ", os.release());
