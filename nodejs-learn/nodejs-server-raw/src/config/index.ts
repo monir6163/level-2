3f8531268a2d0a68e6c9b79637647f0bce294a5d
monir6163 <monirhossain6163@gmail.com>
2025-11-24 18:59:33 +0600

    env secure config

--- nodejs-learn/nodejs-server-raw/src/config/index.ts
+++ nodejs-learn/nodejs-server-raw/src/config/index.ts
@@ -1,3 +1,9 @@
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const config = {
+  app_mode: process.env.NODE_MODE,
+  port: process.env.PORT ? Number(process.env.PORT) : 8000,
+};
+export default config;
--- nodejs-learn/nodejs-server-raw/src/server.ts
+++ nodejs-learn/nodejs-server-raw/src/server.ts
@@ -1,4 +1,5 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -14,6 +15,6 @@ const server: Server = http.createServer(
     }
   }
 );
-server.listen(5000, () => {
-  console.log(`Server running on Port: 5000`);
+server.listen(config.port, () => {
+  console.log(`Server running on Port: ${config.port}`);
 });
